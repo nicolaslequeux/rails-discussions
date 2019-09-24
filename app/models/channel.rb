f979ef7659916d652d5bde9755a20e8cfe371b8e
nicolas lequeux <contact@nicolaslequeux.com>
2019-09-24 11:06:43 +0200

    added associationsé

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,8 @@
 class Channel < ApplicationRecord
+
+  has_many :discussions
+
+  # this is how we create a jointable...
+  has_many :users, through: :discussions
+
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,8 @@
 class Reply < ApplicationRecord
+
+  belongs_to :discussion
+  belongs_to :user
+
+  validates :reply, presence: true
+
 end
